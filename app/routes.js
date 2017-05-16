355c6d4902466f324fa1f3cf49be1f585db374ff
madHEYsia\submi <submiitr07@gmail.com>
2017-05-16 19:07:01 +0530

    added route and container using npm run generate

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,26 @@ export default function createRoutes(store) {
         importModules.catch(errorLoading);
       },
     }, {
+      path: '/home',
+      name: 'home',
+      getComponent(nextState, cb) {
+        const importModules = Promise.all([
+          import('containers/Home/reducer'),
+          import('containers/Home/sagas'),
+          import('containers/Home'),
+        ]);
+
+        const renderRoute = loadModule(cb);
+
+        importModules.then(([reducer, sagas, component]) => {
+          injectReducer('home', reducer.default);
+          injectSagas(sagas.default);
+          renderRoute(component);
+        });
+
+        importModules.catch(errorLoading);
+      },
+    }, {
       path: '*',
       name: 'notfound',
       getComponent(nextState, cb) {
